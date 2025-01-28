e4fb1e4ebffff30809547a70aa1ba83611cae5c9
Yash <78804783+yashd-dev@users.noreply.github.com>
2025-01-28 22:41:34 +0530

    feat: boilerplate template

---
+++ frontend/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="mx-auto max-w-7xl pb-24 pt-10 sm:grid lg:grid-cols-2 sm:pb-32 lg:gap-x-8 lg:px-8 lg:pt-32 lg:pb-52">
+      <h1 className=" scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Welcome to Datathon Round 2
+      </h1>
+    </div>
+  );
+}
