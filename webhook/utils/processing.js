22e22d716db4bd95b2ad8bb4184733431449577f
Manan Gandhi <ardumanan@gmail.com>
2025-01-31 10:25:32 +0530

    [all] Create filetree route

--- webhook/utils/processing.js
+++ webhook/utils/processing.js
@@ -2,6 +2,7 @@ import {
     generateDiagram,
     generateReadMe,
     generateTestSuite,
+    getFileTree,
     scanRepository,
 } from "../utils/repo-data.js";
 import { set } from "../utils/keyvalue-db.js";
@@ -9,17 +10,19 @@ import sendEmail from "./email.js";
 
 const processData = async (repo, repoData, email) => {
     console.log("Processing");
-    const [scan, testSuites, diagram, readme] = await Promise.all([
+    const [filetree, scan, testSuites, diagram, readme] = await Promise.all([
+        getFileTree(repo, repoData.user.ghAccessToken, false),
         scanRepository(repo, repoData.user.ghAccessToken, false),
         generateTestSuite(repo, repoData.user.ghAccessToken, false),
         generateDiagram(repo, repoData.user.ghAccessToken, false),
         generateReadMe(repo, repoData.user.ghAccessToken, false),
     ]);
 
+    await set(`${repo}:filetree`, JSON.stringify(filetree), 3600 * 3);
     await set(`${repo}:scan`, JSON.stringify(scan), 3600 * 3);
     await set(`${repo}:testsuite`, JSON.stringify(testSuites), 3600 * 3);
     await set(`${repo}:diagram`, JSON.stringify(diagram), 3600 * 3);
-    await set(`${repo}:readme`, JSON.stringify(diagram), 3600 * 3);
+    await set(`${repo}:readme`, JSON.stringify(readme), 3600 * 3);
 
     sendEmail(
         email,
